5ce9507dc9435080b342daa2cc52142fa0c315cb
SounosukeNamamura <23aj106@ms.dendai.ac.jp>
2025-12-22 07:50:07 +0900

    修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import RunningCourseApp from './RunningCourseApp.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
+  <RunningCourseApp />
 )
